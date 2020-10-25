fb77500af2c5443aafd6dea550b5e567da3e900c
samueltrillo <knubbe10@gmail.com>
2020-10-25 19:22:27 -0300

    If Else section resolved

--- exercises.js
+++ exercises.js
@@ -15,7 +15,7 @@ c= a + b;
 var name,surname,final;
 name = "Samuel";
 country = "Trillo";
-final= name + country;
+final = name + country;
 
 
 /* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
@@ -77,7 +77,8 @@ text6 = "iNDUSTRIAL eNGINEERING";
 result4 = text6.substring(text6.indexOf("i"),text6.indexOf("i")+1).toUpperCase()
         + text6.substring(text6.indexOf("i")+1,10).toLocaleLowerCase()
         + " "
-        + text6.substring(text6.indexOf("e"),text6.indexOf("e")+1).toUpperCase() + text6.substring(text6.indexOf("e")+1,22).toLocaleLowerCase();
+        + text6.substring(text6.indexOf("e"),text6.indexOf("e")+1).toUpperCase()
+        + text6.substring(text6.indexOf("e")+1,22).toLocaleLowerCase();
 
 /****************************
   3) Arrays
@@ -115,3 +116,69 @@ months.join("-");
 /* g) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).*/
 
 var monthsCopy = months.slice(4,11);
+
+/****************************
+  4) If Else
+*****************************/  
+
+/* a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar
+una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”*/
+
+if (Math.random() >= 0.5) {
+      alert ("Greater than 0,5");
+}   else {
+      alert ("Lower than 0,5");
+}
+
+/* b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+            i) “Bebe” si la edad es menor a 2 años
+            ii) “Nino” si la edad es entre 2 y 12 años
+            iii) “Adolecente” si la edad es entre 13 y 19 años
+            iv) “Joven” si la edad está entre 20 y 30 años
+            v) “Adulto” entre 31 y 60 años
+            vi) “Adulto mayor” entre 61 y 75 años
+            vii) “Anciano” si es mayor a 75 años*/
+
+var Age;
+Age = Math.floor(Math.random()*101);
+if (Age < 2) {
+    alert ("Bebe");
+  } else if (Age >= 2 && Age < 12) {
+    alert ("Nino");
+  } else if (Age >= 13 && Age <= 19) {
+    alert ("Adolescente");
+  } else if (Age >= 20 && Age <= 30) {
+    alert ("Joven");
+  } else if (Age >= 31 && Age <= 60) {
+    alert ("Adulto");
+  } else if (Age >= 61 && Age < 75) {
+    alert ("Adulto mayor");
+  } else {
+    alert ("Anciano");
+};
+
+/****************************
+  5) For
+*****************************/  
+
+/* a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una
+alerta utilizando cada una de las palabras.*/
+
+var words;
+words = ["red","blue","orange","green","yellow"];
+for (var i = 0; i <=4; i++) {
+        alert (words[i]);
+}
+
+/* b) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.*/
+
+/* c) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for 
+para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.*/
+
+/* d) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es decir que 
+al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. 
+Mostrar por la consola del navegador el al array final (utilizar console.log).*/
+
+
+
+
