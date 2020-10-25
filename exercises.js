47e8e561b4c4ce87c9ae2f8c2b2a2324f30fc32d
samueltrillo <knubbe10@gmail.com>
2020-10-25 13:59:24 -0300

    First part added

---
+++ exercises.js
@@ -0,0 +1,27 @@
+/****************************
+  1) Variables y Operadores 
+*****************************/
+
+/* a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos
+números en una 3er variable.*/
+
+var a,b,c;
+a = 2;
+b = 3;
+c= a + b;
+
+/* b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+
+var name,surname,final;
+name = "Samuel";
+country = "Trillo";
+final= name + country;
+
+
+/* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado la suma una 3er variable (utilizar length).*/
+
+var firstw,secondw,thirdw;
+firstw = "Hello";
+secondw = "World";
+thirdw = firstw.length + secondw.length;
