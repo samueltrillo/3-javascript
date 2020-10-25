1f5ffe645b2d508d5c6a0c9b457aab17e1c65609
samueltrillo <knubbe10@gmail.com>
2020-10-25 16:20:15 -0300

    Array section resolved

--- exercises.js
+++ exercises.js
@@ -79,3 +79,39 @@ result4 = text6.substring(text6.indexOf("i"),text6.indexOf("i")+1).toUpperCase()
         + " "
         + text6.substring(text6.indexOf("e"),text6.indexOf("e")+1).toUpperCase() + text6.substring(text6.indexOf("e")+1,22).toLocaleLowerCase();
 
+/****************************
+  3) Arrays
+*****************************/        
+
+/* a) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+"Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log)*/
+
+var months = ["Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+              "Agosto", "Septiembre","Octubre", "Noviembre", "Diciembre"];
+console.log (months[4],months[10]); /* I supossed that exercise refeers to May (5) and November (11) */
+
+/* b) Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
+
+console.log(months.sort());
+
+/* c)Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
+
+months.unshift("Hello");
+months.push("Bye");
+
+/* d) Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
+
+months.shift("Hello");
+months.pop("Bye");
+
+/* e) Invertir el orden del array (utilizar reverse)*/
+
+months.reverse();
+
+/* f) Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).*/
+
+months.join("-");
+
+/* g) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).*/
+
+var monthsCopy = months.slice(4,11);
